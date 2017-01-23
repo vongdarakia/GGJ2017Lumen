3a5dc475cb78bb480322b6b5fb779345a022e65a
vongdarakia <vongdarakia@gmail.com>
2017-01-23 08:32:01 -0800

    everything in assets

---
+++ Assets/logoscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogoScript : MonoBehaviour {
+	float time;
+	public Image myImage;
+	Color fadeColor;
+
+	// Use this for initialization
+	void Start () {
+		time = 3.0f;
+		fadeColor = new Color (1f, 1f, 1f, 0f);
+		myImage.CrossFadeColor (fadeColor, time, true, true);
+
+	}
+}
